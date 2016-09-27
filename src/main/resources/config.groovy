da6069acd7b20e86d7559f442a687665720274c0
zcfrank1st <zcfrank1st@sina.com>
2016-09-27 15:20:20 +0800

    add skeleton

---
+++ src/main/resources/config.groovy
@@ -0,0 +1,25 @@
+environments {
+    dev {
+        jdbc {
+            url = 'development'
+            user = 'xxxx'
+            password = 'xxxx'
+        }
+    }
+
+    test {
+        jdbc {
+            url = 'test'
+            user = 'xxxx'
+            password = 'xxxx'
+        }
+    }
+
+    prd {
+        jdbc {
+            url = 'production'
+            user = 'xxxx'
+            password = 'xxxx'
+        }
+    }
+}
